67a0016d4660d462110ed348d69a38e820ac1b48
pranjal yadav <yashydv0677@gmail.com>
2026-08-14 19:46:38 +0530

    Add Reverse Linked List solution

--- String/CountNumberofSubstrings.cpp
+++ String/CountNumberofSubstrings.cpp
@@ -1,28 +1,25 @@
 #include<iostream>
-#include<bits/stdc++.h>
-using namespace std ;
-int atmost(string s ,int k){
+#include<unordered_map>
+using namespace std;
+int atmost(string s , int k){
+    int left=0;
+    int ans=0;
 unordered_map<char,int> freq;
-int left=0;
-int ans=0;
 for(int b=0;b<s.length();b++){
     freq[s[b]]++;
     while(freq.size()>k){
-        freq[s[left]]--;
-        if(freq[s[left]]==0)freq.erase(s[left]);
-        left++;
+     freq[s[left]]--;
+     if(freq[s[left]]==0)freq.erase(s[left]);
+     left++;
     }
-    ans += b-left+1;
+    ans+= b-left+1;
 }
 return ans;
 }
 int main(){
     string s="pqpqs";
-    int k;
+    int k ;
     cin>>k;
-    int left=0;
-    vector<int> fre(26,0);
-    
-   int ans=atmost(s,k)-atmost(s,k-1);
-cout<<ans;
+    int ans=atmost(s,k)-atmost(s,k-1);
+    cout<<ans;
 }
