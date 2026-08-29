6000859acb2efa7a43da95c84ffcf18c606e071e
pranjal yadav <yashydv0677@gmail.com>
2026-08-29 08:12:59 +0530

    Combination_Sum

---
+++ Recursion/Combination_Sum.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(vector<int> &arr, int target, int idx,
+           vector<int> &curr, vector<vector<int>> &ans) {
+
+    if (target == 0) {
+        ans.push_back(curr);
+        return;
+    }
+
+    if (idx == arr.size() || target < 0)
+        return;
+
+    curr.push_back(arr[idx]);
+    solve(arr, target - arr[idx], idx , curr, ans);
+    curr.pop_back();
+
+    solve(arr, target, idx + 1, curr, ans);
+}
+
+int main() {
+    vector<int> arr = {4, 9, 2, 5, 1};
+
+    int target;
+    cin >> target;
+
+    vector<vector<int>> ans;
+    vector<int> curr;
+
+    solve(arr, target, 0, curr, ans);
+
+    for (auto final : ans) {
+        for (auto x : final)
+            cout << x << " ";
+        cout << "\n";
+    }
+}
