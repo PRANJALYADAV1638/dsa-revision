5d06077c85219e901cbff37e5355d206407ee535
pranjal yadav <yashydv0677@gmail.com>
2026-09-15 23:26:24 +0530

    Infix_to_Postfix

---
+++ STACK_AND_QUEUE/Infix_to_Postfix.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int precedence(char op) {
+    if (op == '^')
+        return 3;
+    else if (op == '*' || op == '/')
+        return 2;
+    else if (op == '+' || op == '-')
+        return 1;
+
+    return 0;
+}
+
+string infixToPostfix(string s) {
+
+    stack<char> st;
+    string ans = "";
+
+    for (int i = 0; i < s.length(); i++) {
+
+        char ch = s[i];
+
+        if (isalnum(ch)) {
+            ans += ch;
+        }
+
+        else if (ch == '(') {
+            st.push(ch);
+        }
+
+        else if (ch == ')') {
+
+            while (!st.empty() && st.top() != '(') {
+                ans += st.top();
+                st.pop();
+            }
+
+            if (!st.empty()) {
+                st.pop();   
+            }
+        }
+
+        else {
+
+            while (!st.empty() &&
+                   st.top() != '(' &&
+                   precedence(st.top()) >= precedence(ch)) {
+
+                ans += st.top();
+                st.pop();
+            }
+
+            st.push(ch);
+        }
+    }
+
+    while (!st.empty()) {
+        ans += st.top();
+        st.pop();
+    }
+
+    return ans;
+}
+
+int main() {
+
+    string infix;
+
+    cout << "Enter infix expression: ";
+    cin >> infix;
+
+    cout << "Postfix expression: "
+         << infixToPostfix(infix) << endl;
+
+    return 0;
+}
