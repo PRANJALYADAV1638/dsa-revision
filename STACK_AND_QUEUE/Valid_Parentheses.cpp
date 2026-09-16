37729b55f4c69bab86898494516c013db2fed241
pranjal yadav <yashydv0677@gmail.com>
2026-09-16 20:45:21 +0530

    Valid_Parentheses

---
+++ STACK_AND_QUEUE/Valid_Parentheses.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            }
+
+            else if (s[i] == ')') {
+                if (st.empty() || st.top() != '(')
+                    return false;
+                st.pop();
+            }
+
+            else if (s[i] == '}') {
+                if (st.empty() || st.top() != '{')
+                    return false;
+                st.pop();
+            }
+
+            else if (s[i] == ']') {
+                if (st.empty() || st.top() != '[')
+                    return false;
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
+
+int main() {
+
+    Solution obj;
+
+    string s;
+    cout << "Enter brackets: ";
+    cin >> s;
+
+    if (obj.isValid(s)) {
+        cout << "Valid";
+    }
+    else {
+        cout << "Invalid";
+    }
+
+    return 0;
+}
